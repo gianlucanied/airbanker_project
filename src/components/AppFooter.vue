93bc851129ba2be38b88eefdb8b4f65176f6dc3c
gianlucanied <gianluca.nieddu96@gmail.com>
2024-10-16 17:54:57 +0200

    fix: footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,64 +5,168 @@ import "aos/dist/aos.css"; // Importa il CSS di AOS
 export default {
   name: "AppFooter",
   mounted() {
-    AOS.init();
+    AOS.init(); // Inizializza AOS
+    // Inizializzazione di particles.js
+    particlesJS("particles-js", {
+      particles: {
+        number: {
+          value: 200,
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000",
+          },
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 5,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: true,
+            mode: "grab",
+          },
+          onclick: {
+            enable: true,
+            mode: "push",
+          },
+          resize: true,
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      retina_detect: true,
+    });
   },
 };
 </script>
 
 <template>
-  <body>
-    <footer class="footer" data-aos="flip-up" data-aos-duration="1000">
-      <div class="logo">
-        <img src="/public/logo_3-removebg.png" alt="" />
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="footer-col">
-            <h4>company</h4>
-            <ul>
-              <li><a href="#">about us</a></li>
-              <li><a href="#">our services</a></li>
-              <li><a href="#">privacy policy</a></li>
-              <li><a href="#">affiliate program</a></li>
-            </ul>
-          </div>
-          <div class="footer-col">
-            <h4>get help</h4>
-            <ul>
-              <li><a href="#">FAQ</a></li>
-              <li><a href="#">shipping</a></li>
-              <li><a href="#">returns</a></li>
-              <li><a href="#">order status</a></li>
-              <li><a href="#">payment options</a></li>
-            </ul>
-          </div>
-          <div class="footer-col">
-            <h4>online shop</h4>
-            <ul>
-              <li><a href="#">watch</a></li>
-              <li><a href="#">bag</a></li>
-              <li><a href="#">shoes</a></li>
-              <li><a href="#">dress</a></li>
-            </ul>
-          </div>
-          <div class="footer-col">
-            <h4>follow us</h4>
-            <div class="social-links">
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-              <a href="#"><i class="fab fa-linkedin-in"></i></a>
-            </div>
+  <footer class="footer" data-aos="flip-up" data-aos-duration="1000">
+    <div id="particles-js"></div>
+    <!-- Div per particles, dentro il footer -->
+    <div class="logo-img">
+      <img src="/public/logo_3-removebg.png" alt="Logo" />
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>company</h4>
+          <ul>
+            <li><a href="#">about us</a></li>
+            <li><a href="#">our services</a></li>
+            <li><a href="#">privacy policy</a></li>
+            <li><a href="#">affiliate program</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>get help</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">shipping</a></li>
+            <li><a href="#">returns</a></li>
+            <li><a href="#">order status</a></li>
+            <li><a href="#">payment options</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>online shop</h4>
+          <ul>
+            <li><a href="#">watch</a></li>
+            <li><a href="#">bag</a></li>
+            <li><a href="#">shoes</a></li>
+            <li><a href="#">dress</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
           </div>
         </div>
       </div>
-    </footer>
-  </body>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
+
 body {
   line-height: 1.5;
   font-family: "Poppins", sans-serif;
@@ -83,24 +187,42 @@ body {
 ul {
   list-style: none;
 }
+
+#particles-js {
+  position: absolute; /* Posiziona il div dietro al contenuto */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0; /* Assicurati che il canvas delle particles stia dietro */
+  pointer-events: none; /* Permette agli eventi del puntatore di passare attraverso */
+}
 .footer {
   background-color: #5bb7b2;
   padding: 70px 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  position: relative;
+  z-index: 1;
 }
-.logo {
-  margin-bottom: 40px;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+.footer img {
+}
+
+.logo-img {
+  background-color: white;
+  padding: 10px;
   border-radius: 15px;
+  margin: 0 auto; /* Centra l'immagine */
+  margin-bottom: 50px;
+  text-align: center; /* Aggiunto per centrare il contenuto */
+  width: fit-content;
+  height: auto;
 }
-.logo img {
-  background-color: white; /* Sfondo bianco per il logo */
-  padding: 10px; /* Aggiungi un po' di spazio intorno al logo */
-  border-radius: 15px; /* Optional: aggiunge un effetto di arrotondamento agli angoli */
-  max-width: 250px; /* Imposta una larghezza massima per mantenere il logo proporzionato */
+
+.logo-img img {
+  width: 250px; /* Mantieni la larghezza del logo */
+  border-radius: 10px; /* Arrotonda i bordi dell'immagine */
 }
+
 .footer-col {
   width: 25%;
   padding: 0 15px;
@@ -132,7 +254,6 @@ ul {
   color: #ffffff;
   text-decoration: none;
   font-weight: 300;
-  color: #ffffff;
   display: block;
   transition: all 0.3s ease;
 }
@@ -155,18 +276,5 @@ ul {
 .footer-col .social-links a:hover {
   color: #24262b;
   background-color: #ffffff;
-}
-
-/*responsive*/
-@media (max-width: 767px) {
-  .footer-col {
-    width: 50%;
-    margin-bottom: 30px;
-  }
-}
-@media (max-width: 574px) {
-  .footer-col {
-    width: 100%;
-  }
 }
 </style>
